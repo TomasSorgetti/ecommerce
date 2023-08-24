e4d49a0324f2a063ae0610d6ed863bbe97f5118a
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2023-08-24 18:03:14 -0300

    folders

---
+++ e-commerce/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Hola Mundo!</h1>
+    </main>
+  )
+}
+
+export default Home
---
+++ e-commerce/src/pages/Login/Create/Validation.js
@@ -0,0 +1,35 @@
+const validate = (form, errorsState) => {
+  const errors = { ...errorsState };
+
+  // name
+  if (!form.name) errors.name = "";
+  else if (form.name.length < 3) errors.name = "Debe de tener mas de 3 caracteres";
+  else {
+    errors.name = "";
+    }
+    
+  // lastname
+  if (!form.lastname) errors.lastname = "";
+  else if (form.lastname.length < 3)
+    errors.lastname = "Debe de tener mas de 3 caracteres";
+  else {
+    errors.lastname = "";
+    }
+    
+
+  //password
+  if (!form.password) errors.password = "";
+  else if (form.password.length < 6)
+    errors.password = "Debe de tener mas de 6 letras";
+  else errors.password = "";
+
+  //confirm pasword
+  if (!form.repeatPassword) errors.repeatPassword = "";
+  else if (form.repeatPassword !== form.password)
+    errors.repeatPassword = "Las contraseñas no coinciden";
+  else errors.repeatPassword = "";
+    
+  return errors;
+};
+
+export default validate;
